5863216bda07473043cc77ff41e791692459e558
EmilyPlatzer <emilyplatzer@gmail.com>
2014-06-18 09:25:48 -0600

    User can determine differences in an array of grades

---
+++ lib/grading.rb
@@ -0,0 +1,11 @@
+class Grading
+  def grade(grades)
+    diff = []
+    grades.each_with_index do |grade, index|
+      diff << :up if grades[index+1] && grade < grades[index+1]
+      diff << :even if grades[index+1] && grade == grades[index+1]
+      diff << :down if grades[index+1] && grade > grades[index+1]
+    end
+    diff
+  end
+end
---
+++ spec/grading_spec.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+require 'grading'
+
+describe 'grading' do
+  it 'describes a difference in grades' do
+    grades = Grading.new
+    actual = grades.grade([6,3,5,4,3,4,4,5])
+    expected = [:down,:up,:down,:down,:up,:even,:up]
+    expect(actual).to eq(expected)
+  end
+end
