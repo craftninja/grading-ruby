c71498fc2436fd300067d3ede2b4686b99faba1f
EmilyPlatzer <emilyplatzer@gmail.com>
2014-06-18 09:44:55 -0600

    User can find students with declining grades

--- lib/grading.rb
+++ lib/grading.rb
@@ -8,4 +8,15 @@ class Grading
     end
     diff
   end
+
+  def decline?(grades)
+    diffs = differences(grades)
+    down_in_row = 0
+    diffs.each do |diff|
+      down_in_row += 1 if diff == :down
+      down_in_row = 0 if diff == :up
+    end
+    down_in_row >= 3
+  end
+
 end
--- spec/grading_spec.rb
+++ spec/grading_spec.rb
@@ -8,4 +8,32 @@ describe 'grading' do
     expected = [:down, :up, :down, :down, :up, :even, :up]
     expect(actual).to eq(expected)
   end
+
+  it 'states that a student is in decline when they have three declining grades in a row' do
+    grades = Grading.new
+    actual = grades.decline?([10, 9, 8, 7])
+    expected = true
+    expect(actual).to eq(expected)
+  end
+
+  it 'states that a student is in decline when the have a trend of three declining grades (interspersed with evens)' do
+    grades = Grading.new
+    actual = grades.decline?([10, 10, 10, 9, 9, 8, 8, 8, 8, 7])
+    expected = true
+    expect(actual).to eq(expected)
+  end
+
+  it 'states that a student is not in decline when they have an inclinging grade after three declines' do
+    grades = Grading.new
+    actual = grades.decline?([10, 9, 8, 7, 8])
+    expected = false
+    expect(actual).to eq(expected)
+  end
+
+  it 'states that a student is not in decline when they have a trend of two declines' do
+    grades = Grading.new
+    actual = grades.decline?([10, 9, 8])
+    expected = false
+    expect(actual).to eq(expected)
+  end
 end
